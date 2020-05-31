b1cdd328937e3fdaf2e6d25d6ddbc8f61da91d68
mattopsh <229755@student.pwr.edu.pl>
2020-05-31 08:59:34 +0200

    provider calls implementation, add auth calls modules for dagger

---
+++ app/src/main/java/com/hfad/parkingfinder/restclient/provider/dto/ProviderDto.kt
@@ -0,0 +1,24 @@
+package com.hfad.parkingfinder.restclient.provider.dto
+
+data class ProviderDto(val providerNodeId: Long,
+                       val providerName: String,
+                       val providerDetails: String,
+                       val attitude: Double,
+                       val longitude: Double,
+                       val distance: Int,
+                       val openingHours: String?,
+                       val providerType: ProviderType?,
+                       val exactProviderType: String?,
+                       val url: String?)
+
+enum class ProviderType {
+    FOOD,
+    DIY,
+    CLOTHES,
+    BEAUTY,
+    INTERIOR,
+    ELECTRONICS,
+    SPORT,
+    CAR,
+    ENTMT
+}
